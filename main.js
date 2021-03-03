bbc076a18d31c7112c993d6cf757bf3f5840eac6
Catalina Boznea <catalina.boznea@tradeshift.com>
2021-03-03 17:40:12 +0100

    Bind attributes

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		product: 'Socks',
-		description: 'A pair of warm, fuzzy socks.'
+		description: 'A pair of warm, fuzzy socks.',
+		image: './assets/vmSocks-green-onWhite.jpg',
+		altText: 'A pari of socks',
+		link: 'https://en.wikipedia.org/wiki/Sock'
 	}
 });
